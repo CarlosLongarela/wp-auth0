b83952b7a8523101548b5822cd35241d64409093
Josh Cunningham <josh@joshcanhelp.com>
2018-08-09 11:04:27 -0700

    Add clear log action; combine same errors in log; increase error log limit; fix offline assets/styles

--- lib/WP_Auth0_ErrorLog.php
+++ lib/WP_Auth0_ErrorLog.php
@@ -1,16 +1,85 @@
 <?php
+/**
+ * Contains the WP_Auth0_LoginManager class.
+ *
+ * @package WP-Auth0
+ */
+
+/**
+ * Class WP_Auth0_ErrorLog.
+ * Handles error log CRUD actions and hooks.
+ */
 class WP_Auth0_ErrorLog {
 
 	/**
+	 * Option name used to store the error log.
+	 */
+	const OPTION_NAME = 'auth0_error_log';
+
+	/**
+	 * Add actions and filters for the error log settings section.
 	 *
-	 * @deprecated 3.6.0 - Not needed, handled in WP_Auth0_Admin::admin_enqueue()
+	 * @link https://developer.wordpress.org/reference/hooks/admin_action__requestaction/
 	 */
 	public function init() {
-		// phpcs:ignore
-		trigger_error( sprintf( __( 'Method %s is deprecated.', 'wp-auth0' ), __METHOD__ ), E_USER_DEPRECATED );
+		add_action( 'admin_action_wpauth0_clear_error_log', 'wp_auth0_errorlog_clear_error_log' );
 	}
 
 	/**
+	 * Render the settings page.
+	 *
+	 * @see WP_Auth0_Settings_Section::init_menu()
+	 */
+	public function render_settings_page() {
+		include WPA0_PLUGIN_DIR . 'templates/a0-error-log.php';
+	}
+
+	/**
+	 * Get the error log.
+	 *
+	 * @return array
+	 */
+	public function get() {
+		$log = get_option( self::OPTION_NAME );
+
+		if ( empty( $log ) ) {
+			$log = array();
+		}
+
+		return $log;
+	}
+
+	/**
+	 * Update the error log with an array.
+	 *
+	 * @param array $log - Log array to update.
+	 *
+	 * @return bool
+	 */
+	public function update( array $log ) {
+		return update_option( self::OPTION_NAME, $log );
+	}
+
+	/**
+	 * Clear out the error log.
+	 *
+	 * @return bool
+	 */
+	public function clear() {
+		return update_option( self::OPTION_NAME, array() );
+	}
+
+	/**
+	 * Delete the error log option.
+	 *
+	 * @return bool
+	 */
+	public function delete() {
+		return delete_option( self::OPTION_NAME );
+	}
+
+	/**
+	 * Enqueue scripts and styles.
 	 *
 	 * @deprecated 3.6.0 - Not needed, handled in WP_Auth0_Admin::admin_enqueue()
 	 */
@@ -18,12 +87,26 @@ class WP_Auth0_ErrorLog {
 		// phpcs:ignore
 		trigger_error( sprintf( __( 'Method %s is deprecated.', 'wp-auth0' ), __METHOD__ ), E_USER_DEPRECATED );
 	}
+}
 
-	public function render_settings_page() {
+/**
+ * Function to call the method that clears out the error log.
+ *
+ * @hook admin_action_wpauth0_clear_error_log
+ */
+function wp_auth0_errorlog_clear_error_log() {
 
-		$data = get_option( 'auth0_error_log', array() );
-
-		include WPA0_PLUGIN_DIR . 'templates/a0-error-log.php';
+	if ( empty( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'clear_error_log' ) ) {
+		wp_die( __( 'Not allowed.', 'wp-auth0' ) );
 	}
 
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'Not authorized.', 'wp-auth0' ) );
+	}
+
+	$error_log = new WP_Auth0_ErrorLog();
+	$error_log->clear();
+
+	wp_safe_redirect( admin_url( 'admin.php?page=wpa0-errors&cleared=1' ) );
+	exit;
 }
--- lib/WP_Auth0_ErrorManager.php
+++ lib/WP_Auth0_ErrorManager.php
@@ -1,56 +1,78 @@
 <?php
+/**
+ * Contains the WP_Auth0_ErrorManager class.
+ *
+ * @package WP-Auth0
+ */
 
 /**
- * Class WP_Auth0_ErrorManager
+ * Class WP_Auth0_ErrorManager.
+ * Handles creating a new error log entry.
  */
 class WP_Auth0_ErrorManager {
 
 	/**
-	 * Create a row in the error log, up to 20 entries
+	 * Create a row in the error log.
 	 *
-	 * @param string                    $section - portion of the codebase that generated the error
-	 * @param string|WP_Error|Exception $error - error message string or discoverable error type
+	 * @param string $section - Portion of the codebase that generated the error.
+	 * @param mixed  $error - Error message string or discoverable error type.
+	 *
+	 * @return bool
 	 */
 	public static function insert_auth0_error( $section, $error ) {
-		$code    = 'unknown_code';
-		$message = __( 'Unknown error message', 'wp-auth0' );
 
-		if ( $error instanceof WP_Error ) {
-			$code    = $error->get_error_code();
-			$message = $error->get_error_message();
-		} elseif ( $error instanceof Exception ) {
-			$code    = $error->getCode();
-			$message = $error->getMessage();
-		} elseif ( is_array( $error ) && ! empty( $error['response'] ) ) {
-			if ( ! empty( $error['response']['code'] ) ) {
-				$code = sanitize_text_field( $error['response']['code'] );
-			}
-			if ( ! empty( $error['response']['message'] ) ) {
-				$message = sanitize_text_field( $error['response']['message'] );
-			}
-		} else {
-			$message = is_object( $error ) || is_array( $error ) ? serialize( $error ) : $error;
-		}
-
-		$log = get_option( 'auth0_error_log' );
-
-		if ( empty( $log ) ) {
-			$log = array();
-		}
-
-		array_unshift(
-			$log, array(
-				'section' => $section,
-				'code'    => $code,
-				'message' => $message,
-				'date'    => time(),
-			)
+		$new_entry = array(
+			'section' => $section,
+			'code'    => 'unknown_code',
+			'message' => __( 'Unknown error message', 'wp-auth0' ),
 		);
 
-		if ( count( $log ) > 20 ) {
+		if ( $error instanceof WP_Error ) {
+			$new_entry['code']    = $error->get_error_code();
+			$new_entry['message'] = $error->get_error_message();
+		} elseif ( $error instanceof Exception ) {
+			$new_entry['code']    = $error->getCode();
+			$new_entry['message'] = $error->getMessage();
+		} elseif ( is_array( $error ) && ! empty( $error['response'] ) ) {
+			if ( ! empty( $error['response']['code'] ) ) {
+				$new_entry['code'] = sanitize_text_field( $error['response']['code'] );
+			}
+			if ( ! empty( $error['response']['message'] ) ) {
+				$new_entry['message'] = sanitize_text_field( $error['response']['message'] );
+			}
+		} else {
+			$new_entry['message'] = is_object( $error ) || is_array( $error ) ? serialize( $error ) : $error;
+		}
+
+		$error_log = new WP_Auth0_ErrorLog();
+		$log       = $error_log->get();
+
+		// Prepare the last error log entry to compare with the new one.
+		$last_entry = null;
+		if ( ! empty( $log ) ) {
+			// Get the last error logged.
+			$last_entry = $log[0];
+
+			// Remove date and count fields so it can be compared with the new error.
+			$last_entry = array_diff_key( $last_entry, array_flip( array( 'date', 'count' ) ) );
+		}
+
+		if ( serialize( $last_entry ) === serialize( $new_entry ) ) {
+			// New error and last error are the same so set the current time and increment the counter.
+			$log[0]['date']  = time();
+			$log[0]['count'] = isset( $log[0]['count'] ) ? intval( $log[0]['count'] ) + 1 : 2;
+		} else {
+			// New error is not a repeat to set required fields.
+			$new_entry['date']  = time();
+			$new_entry['count'] = 1;
+			array_unshift( $log, $new_entry );
+		}
+
+		// Make sure we stay under 30 logged errors.
+		if ( count( $log ) > 30 ) {
 			array_pop( $log );
 		}
 
-		update_option( 'auth0_error_log', $log );
+		return $error_log->update( $log );
 	}
 }
